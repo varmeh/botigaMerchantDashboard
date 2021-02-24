c3899131b00b6fae2cb8274564679a633d39b594
Rahul <Rahul@botiga.app>
2021-02-25 00:30:18 +0530

    adding the url based on env

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { Token } from "./Token";
 
-const instance = axios.create({ baseURL: 'https://dev.botiga.app' });
+const instance = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 const token = new Token();
 token.initAuthenticationToken();
 
-export default function () {
+export default function axiosConfig() {
     instance.defaults.headers.common['Authorization'] = token.getAuthenticationToken();
     instance.defaults.headers.post['Content-Type'] = 'application/json';
     instance.defaults.headers.post['Accept'] = 'application/json';
