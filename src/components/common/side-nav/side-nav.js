1d651cfd6c7e5edc9c7215f8b4dcea32f0feb97d
Rahul <Rahul@botiga.app>
2021-03-06 22:37:21 +0530

    initila commit for delivery

--- src/components/common/side-nav/side-nav.js
+++ src/components/common/side-nav/side-nav.js
@@ -4,12 +4,13 @@ import { NavLink, withRouter } from 'react-router-dom';
 import botigaLogo from "../../../assets/icons/botiga_logo.svg";
 import storeIcon from "../../../assets/icons/store.svg";
 import promoIcon from "../../../assets/icons/promo.svg";
+import deliveryIcon from "../../../assets/icons/delivery.svg";
 import logoutIcon from "../../../assets/icons/logout.svg";
 
 import { Logout } from "../../../services/auth-service";
 import { Token } from "../../../helpers/Token";
 import AppContext from "../../../contexts/AppContext";
-import { LOGIN_VIEW, STORE_VIEW, PROMO_VIEW } from "../../../helpers/BotigaRouteFile";
+import { LOGIN_VIEW, STORE_VIEW, PROMO_VIEW, DELIVERY_VIEW } from "../../../helpers/BotigaRouteFile";
 
 import "./side-nav.css";
 export const SideNav = withRouter(({ history }) => {
@@ -32,6 +33,7 @@ export const SideNav = withRouter(({ history }) => {
             <img className="botiga_logo" alt="botiga-logo" src={botigaLogo} />
             <MenuIconItem image={storeIcon} text={"Store"} to={STORE_VIEW} />
             <MenuIconItem image={promoIcon} text={"Promos"} to={PROMO_VIEW} />
+            <MenuIconItem image={deliveryIcon} text={"Delivery"} to={DELIVERY_VIEW} />
             <MenuIconItem image={logoutIcon} text={"Logout"} isLogout handleLogout={handleLogout} />
         </div>
     );
--- src/helpers/BotigaRouteFile.js
+++ src/helpers/BotigaRouteFile.js
@@ -2,5 +2,6 @@ export const LOGIN_VIEW = '/';
 export const VERIFY_OTP_VIEW = '/verify-otp';
 export const STORE_VIEW = '/store';
 export const PROMO_VIEW = '/promos';
+export const DELIVERY_VIEW = '/delivery';
 
 export const HOME_VIEW = STORE_VIEW;
